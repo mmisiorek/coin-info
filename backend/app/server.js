172286b00c1004fd65afc315890734eb66598682
Marcin Misiorek <marcin@misiorek.net>
2018-02-05 13:52:31 +0100

    Add lint command to package.json

--- backend/app/server.js
+++ backend/app/server.js
@@ -51,19 +51,23 @@ app.listen(process.env.APPLICATION_PORT, () => {
     if (process.env.APPLICATION_MODE === 'development') {
         const web3 = web3Provider.get();
 
-        web3.eth.getAccounts((err, accounts) => {
-            let promise = Promise.resolve();
+        web3.eth.getBlockNumber((blockNumber) => {
+            if (blockNumber === 0) {
+                web3.eth.getAccounts((err, accounts) => {
+                    let promise = Promise.resolve();
 
-            for (let i = 0; i < 10; i += 1) {
-                const chosenAccounts = utils.getRandomAccounts(accounts);
+                    for (let i = 0; i < 10; i += 1) {
+                        const chosenAccounts = utils.getRandomAccounts(accounts);
 
-                promise = promise.then(() =>
-                    utils.getTransferPromise(chosenAccounts.from, chosenAccounts.to, 2000));
+                        promise = promise.then(() =>
+                            utils.getTransferPromise(chosenAccounts.from, chosenAccounts.to, 2000));
+                    }
+
+                    promise.then(() => {
+                        console.log('Test transactions are save to the blockchain.');
+                    });
+                });
             }
-
-            promise.then(() => {
-                console.log('Test transactions are save to the blockchain.');
-            });
         });
     }
     /* eslint-enable no-console */
